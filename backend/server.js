6a40e15b3af3d9dacc90c85eaf63ef828c964516
Ajey25 <ajay.prajapati1630as@gmail.com>
2026-02-21 14:11:22 +0530

    Configured Vercel fullstack deployment

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,4 @@ app.get(/^(?!\/api).*$/, (req, res) => {
 });
 
 // Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+export default app;
