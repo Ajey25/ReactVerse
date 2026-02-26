d332b68d434c3bc9c58badcbf785973e1a7c6194
Ajey25 <ajay.prajapati1630as@gmail.com>
2026-02-26 15:44:57 +0530

    updated footer

--- REACTVERSE/src/components/Footer.jsx
+++ REACTVERSE/src/components/Footer.jsx
@@ -7,14 +7,29 @@ export function Footer() {
         </div>
 
         <div className="flex gap-4">
-          <a href="#" className="text-white hover:text-gray-300">
+          <a
+            href="https://github.com/Ajey25/ReactVerse"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-300"
+          >
             GitHub
           </a>
-          <a href="#" className="text-white hover:text-gray-300">
-            Docs
+
+          <a
+            href="https://www.linkedin.com/in/ajay-prajapati-4912981b3/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-300"
+          >
+            LinkedIn
           </a>
-          <a href="#" className="text-white hover:text-gray-300">
-            Contact
+
+          <a
+            href="mailto:reactverseproject@gmail.com"
+            className="text-white hover:text-gray-300"
+          >
+            Email
           </a>
         </div>
       </div>
